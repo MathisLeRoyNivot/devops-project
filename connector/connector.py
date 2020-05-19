32752600036f92cc1336692660bfc3713b9693f5
mathislrn <mathis.lrn061198@gmail.com>
2020-05-19 11:27:51 +0200

    fix(gpg): updating connector & main.py

--- connector/connector.py
+++ connector/connector.py
@@ -24,32 +24,44 @@ async def response(websockets, path):
     gpg = gnupg.GPG(gnupghome="/root/.gnupg")
     data = await websockets.recv()
 
-    output_file= "receive_file.json"
-    
     file_name = "json_file.json.gpg"
-    file = open(file_name, "w")
+    file = open(file_name, "wb")
     file.write(data)
+    file.close()
+
+    file_test = open(file_name, "rb")
+
+
+    output_file= "receive_file.json"
 
     with open(file_name, 'rb') as f:
         status = gpg.decrypt_file(f, passphrase='P@ssw0rd')
-    print(status.ok)
-    print(status.status)
-    print(status.stderr)
+    print(f'ok : {status.ok}')
+    print(f'status : {status.status}')
+    print(f'stderr : {status.stderr}')
 
-    decoded_data = json.loads(status)
-    await websockets.send("Data received ✅")
+    logger = logging.getLogger()
+    logger.debug(f'ok : {status.ok}')
+    logger.debug(f'status : {status.status}')
+    logger.debug(f'stderr : {status.stderr}')
+    logger.info(f'data received : {data}')
+    logger.error(f'file content : {file_test.read()}')
 
-    for row in decoded_data:
-        sql_insert_statement = """
-        INSERT INTO data (unit_num, automaton_num, automaton_type, tank_temp, outside_temp, milk_weight, final_product_weight, ph, potassium, sodium_chlorure_concentration, salmonella_lvl, e_coli_lvl, listeria_lvl, created_at, inserted_at)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-        """
-        
-        row_to_insert = (row["unit_num"], row["automaton_num"], row["automaton_type"], row["tank_temp"], row["outside_temp"], row["milk_weigth"], row["final_product_weight"], row["ph"], row["potassium"], row["sodium_chlorure_concentration"], row["salmonella_level"], row["e_coli_level"], row["listeria_level"], row["created_at"], datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        cursor.execute(sql_insert_statement, row_to_insert)
-        mariadb_connection.commit()
+    if status.ok:
+        decoded_data = json.loads(status)
+        await websockets.send("Data received ✅")
 
-    print(f"✅ {len(decoded_data)} row(s) inserted into data table")
+        for row in decoded_data:
+            sql_insert_statement = """
+            INSERT INTO data (unit_num, automaton_num, automaton_type, tank_temp, outside_temp, milk_weight, final_product_weight, ph, potassium, sodium_chlorure_concentration, salmonella_lvl, e_coli_lvl, listeria_lvl, created_at, inserted_at)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """
+            
+            row_to_insert = (row["unit_num"], row["automaton_num"], row["automaton_type"], row["tank_temp"], row["outside_temp"], row["milk_weigth"], row["final_product_weight"], row["ph"], row["potassium"], row["sodium_chlorure_concentration"], row["salmonella_level"], row["e_coli_level"], row["listeria_level"], row["created_at"], datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            cursor.execute(sql_insert_statement, row_to_insert)
+            mariadb_connection.commit()
+
+        print(f"✅ {len(decoded_data)} row(s) inserted into data table")
 
 start_server = websockets.serve(response, HOST, SERVER_PORT)
 print(f"✅ Server started on port : {SERVER_PORT}")
