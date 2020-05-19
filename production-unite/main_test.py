cbf583006295fe94593bb10a56b74fd6340c0cdc
mathislrn <mathis.lrn061198@gmail.com>
2020-05-19 16:29:40 +0200

    feat(tests): adding unittest for production-unit/main.py

--- production-unite/main_test.py
+++ production-unite/main_test.py
@@ -5,37 +5,37 @@ import main
 
 class testMain(unittest.TestCase):
 
-    def test_generateTypeSuccess(self):
-        result = main.generateType(1)
+    def test_generateType(self):
+        automaton = 1
+        result = main.generateType(automaton)
+
         self.assertTrue('str', type(result))
         self.assertEqual('0001', result)
 
 
-    def test_generateTypeFailure(self):
-        result = main.generateType(10)
-        self.assertFalse('str', type(result))
-        self.assertNotEqual('0001', result)
+    def test_generateRandomData(self):
+        values = {
+            "min": 20,
+            "max": 50,
+            "step": 1
+        }
+        result = main.generateRandomData(values["min"], values["max"], values["step"])
 
-
-    def test_generateRandomDataSuccess(self):
-        result = main.generateRandomData(1, 10, 1)
         self.assertTrue('int', type(result))
+        self.assertLessEqual(values["min"], result)
+        self.assertGreaterEqual(values["max"], result)
 
 
-    def test_generateRandomDataFailure(self):
-        result = main.generateRandomData(1, 10, 1)
-        self.assertFalse('int', type(result))
+    def test_generateRandomDataFloat(self):
+        values = {
+            "min": 1.0,
+            "max": 5.5
+        }
+        result = main.generateRandomDataFloat(values["min"], values["max"])
 
-
-    def test_generateRandomDataFloatSucess(self):
-        result = main.generateRandomDataFloat(1.0, 5.5)
         self.assertTrue('float', type(result))
-
-    
-    def test_generateRandomDataFloatFailure(self):
-        result = main.generateRandomDataFloat(1.0, 5.5)
-        self.assertTrue('float', type(result))
-        self.assertTrue('float', type(result))
+        self.assertLessEqual(values["min"], result)
+        self.assertGreaterEqual(values["max"], result)
 
 
 if __name__ == '__main__':
