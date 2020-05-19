5de236232b8229de26baeeb14e75a80192e03619
adrienvaucard <adrien.vaucard@gmail.com>
2020-05-19 16:00:47 +0200

    fix(CICD): Fix Angular Tests on deployment

--- website/karma.conf.js
+++ website/karma.conf.js
@@ -25,10 +25,19 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadless'],
+    browsers: ['Chrome', 'ChromeHeadless', 'Chromium'],
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--no-sandbox',
+          '--remote-debugging-port=9222'
+        ]
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
-
-  process.env.CHROME_BIN = require('puppeteer').executablePath() //this
 };
