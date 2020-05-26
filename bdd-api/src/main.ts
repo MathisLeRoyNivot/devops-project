f323a582e85beeb0b9a81c4daef0573bab0f2e35
adrienvaucard <adrien.vaucard@gmail.com>
2020-05-26 16:51:56 +0200

    feat(BDD API): re-add HTTPS to API

--- bdd-api/src/main.ts
+++ bdd-api/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+var fs = require('file-system');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/ca-key.pem'),
+    cert: fs.readFileSync('./secrets/ca-cert.pem'),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    cors: true, 
+    httpsOptions
+  });
+  
   app.enableCors();
   await app.listen(3333);
 }
